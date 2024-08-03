5a03a366a5b62e78f2313dab1ee43432de1ab487
George <krasavingeorg@gmail.com>
2024-08-03 09:37:12 +0400

    add адапитивность

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cae%$tog2!_enoxyq389^%ut6&_qs)c-*gf4yfrb0t(#x5%g)w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['81.94.156.136']
+ALLOWED_HOSTS = ['127.0.0.1'] #81.94.156.136
 
 # Application definition
 
--- frontend/src/GoogleSpreadsheet.js
+++ frontend/src/GoogleSpreadsheet.js
@@ -4,7 +4,7 @@ const GoogleSpreadsheet = () => {
   return (
     <div style={{ height: '100vh', width: '100%' }}>
       <iframe
-        src="https://docs.google.com/spreadsheets/d/1oSz0ruFkDKquEkPSjQuT9M7hcKOilXzA/edit?gid=2077659328#gid=2077659328"
+        src="https://docs.google.com/spreadsheets/d/1oSz0ruFkDKquEkPSjQuT9M7hcKOilXzA/edit?usp=sharing&ouid=102845798273549706126&rtpof=true&sd=true"
         style={{ border: 0, width: '100%', height: '100%' }}
         allowFullScreen
         title="Google Spreadsheet"
