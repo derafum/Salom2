d30eed2b84d7f1f6e3ce5715be89288dea942ccd
George <krasavingeorg@gmail.com>
2024-08-03 23:11:35 +0400

    add big picture

--- frontend/src/GoogleSpreadsheet.js
+++ frontend/src/GoogleSpreadsheet.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 const GoogleSpreadsheet = () => {
+  // Убедитесь, что ссылка корректна и указывает на опубликованную версию таблицы
+  const iframeSrc = 'https://docs.google.com/spreadsheets/d/1oSz0ruFkDKquEkPSjQuT9M7hcKOilXzA/edit?gid=2077659328#gid=2077659328';
+
   return (
-    <div style={{ height: '100vh', width: '100%' }}>
+    <div style={{ height: '85vh', width: '100%' }}>
       <iframe
-        src="https://docs.google.com/spreadsheets/d/1oSz0ruFkDKquEkPSjQuT9M7hcKOilXzA/edit?usp=sharing&ouid=102845798273549706126&rtpof=true&sd=true"
+        src={iframeSrc}
         style={{ border: 0, width: '100%', height: '100%' }}
         allowFullScreen
         title="Google Spreadsheet"
--- frontend/src/UnitEconomics.js
+++ frontend/src/UnitEconomics.js
@@ -1,44 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import * as XLSX from 'xlsx';
+import React from 'react';
 import './UnitEconomics.css';
 
 const UnitEconomics = () => {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('/unit.xlsx');
-        const arrayBuffer = await response.arrayBuffer();
-        const workbook = XLSX.read(arrayBuffer, { type: 'array' });
-        const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-        const jsonData = XLSX.utils.sheet_to_json(worksheet);
-        setData(jsonData);
-      } catch (error) {
-        console.error('Error fetching the Excel file:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
+  // Ссылка для встраивания таблицы, убедитесь, что правильно настроили публикацию таблицы
+  const iframeSrc = 'https://docs.google.com/spreadsheets/d/1NOAz9B26pGjxWalsKkxxlE99K8VB6BW-/edit?usp=sharing&ouid=102845798273549706126&rtpof=true&sd=true';
 
   return (
     <div className="unit-economics">
       <h2>Юнит Экономика</h2>
-      <table className="excel-table">
-        <thead>
-          <tr>
-            {data.length > 0 && Object.keys(data[0]).map((key) => <th key={key}>{key}</th>)}
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((row, index) => (
-            <tr key={index}>
-              {Object.values(row).map((value, i) => <td key={i}>{value}</td>)}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <iframe
+        src={iframeSrc}
+        width="100%"
+        height="600" // Вы можете настроить высоту
+        frameBorder="0"
+        className="google-sheet-iframe"
+      >
+        Ваш браузер не поддерживает iframe.
+      </iframe>
     </div>
   );
 };
